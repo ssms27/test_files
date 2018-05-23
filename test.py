c15e1c3508c3818c8c801fd288f64356e7fff9b7
ssms27 <ssms27@hotmail.com>
2018-05-22 22:04:12 -0400

    committing test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python2.7
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+ENV = Environment(loader=FileSystemLoader('.'))
+t = ENV.get_template("test.j2")
+
+location = ['DR', 'PROD']
+
+with open('test.yml') as f:
+	r = yaml.load(f)
+
+for i in location:
+	print t.render(ntp=r['Juniper'][i]['ntp'])

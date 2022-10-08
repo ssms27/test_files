b332c9f95cb44640fd922ceca897743fa3fa48c2
ssms27 <ssms27@hotmail.com>
2022-10-08 11:45:57 -0400

    changed how yaml is read - and converted to dict

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-
+#!/Users/ssms27/venv/bin/python3
 import yaml
 from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
+
 ENV = Environment(loader=FileSystemLoader('.'))
 t = ENV.get_template("test.j2")
 
 location = ['DR', 'PROD']
-
-with open('test.yml') as f:
-	r = yaml.load(f)
+r = yaml.safe_load(Path("test.yml").read_text())
 
 for i in location:
 	print(t.render(ntp=r['Juniper'][i]['ntp']))

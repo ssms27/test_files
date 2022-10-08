b10b34f4e78b91b74abec3a1407939b0f3eed0d3
ssms27 <ssms27@hotmail.com>
2022-10-08 19:47:24 -0400

    object hint added

--- test.py
+++ test.py
@@ -6,7 +6,15 @@ ENV = Environment(loader=FileSystemLoader('.'))
 t = ENV.get_template("test.j2")
 
 location = ['DR', 'PROD']
-r = yaml.safe_load(Path("test.yml").read_text())
 
-for i in location:
-	print(t.render(ntp=r['Juniper'][i]['ntp']))
+
+def get_ntp_config() -> object:
+    global location
+    global t
+    r = yaml.safe_load(Path("test.yml").read_text())
+
+    for i in location:
+        print(t.render(name='name', ntp=r['Juniper'][i]['ntp']))
+
+
+get_ntp_config()
